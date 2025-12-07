58035b153d29f8d440b5b9b5b8d0689d87c11151
JimQian2023 <122236062+JimQian2023@users.noreply.github.com>
2025-12-07 01:19:35 -0800

    Added code (currently unreachable) in index.html to make conversation stateful. This is currently not needed for this project's purpose (until respective backend changes are implemented).

--- src/server.py
+++ src/server.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for dev; you can restrict later
+    allow_origins=["*"],  # Can restrict later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
